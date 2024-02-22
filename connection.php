a2f507aadf4be1aa00adef0b5cb9f3be7e873bce
Khan Rasheed <khanrasheed472@hotmail.com>
2024-02-22 20:27:53 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,12 +7,12 @@ $user = DB_USERNAME;
 $pass = DB_PASSWORD;
 
 try {
-    echo "Connection success! " . $e->getMessage();
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Turn on error mode exception for better debugging
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Set default fetch mode to associative array
         PDO::ATTR_EMULATE_PREPARES => false, // Turn off emulation mode for prepared statements
     ]);
+    echo "Connection success! " . $e->getMessage();
 } catch(PDOException $e) {
     echo "Connection Error! " . $e->getMessage();
 }

c4c587c64d51b73dc2f6ad29278c2e9776dbb495
Khan Rasheed <khanrasheed472@hotmail.com>
2024-02-22 20:47:25 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
-include '/var/www/inc/dbinfo.inc';
-
+include_once '/var/www/inc/dbinfo.inc'; // Corrected path
 
 try {
-    // Establish a new PDO connection
+    // Establish a new PDO connection using RDS details from dbinfo.inc
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_DATABASE, DB_USERNAME, DB_PASSWORD, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -17,7 +16,7 @@ try {
     include_once __DIR__ . '/Expense.php';
     include_once __DIR__ . '/Budget.php';
 
-    // Initialize objects
+    // Initialize objects with the PDO connection
     $getFromU = new User($pdo);
     $getFromB = new Budget($pdo);
     $getFromE = new Expense($pdo);
